5c6a43723f68b3273f91c09144ceed529aaa5b7d
scaperoth <mscapero@gwu.edu>
2016-03-16 14:02:21 -0400

    updated documentation

--- PrimeTest.java
+++ PrimeTest.java
@@ -1,6 +1,9 @@
 // @file: PrimeTest.java
 // @author: scaperoth@gmail.com
-// @description: main class to test primes.test
+// @description: main class to test Primes.java
+// loops through from 1-2^n where n is number of iterations
+// and outputs the results at each step
+// 
 //
 package primetest;
 
@@ -9,15 +12,24 @@ import java.util.*;
 public class PrimeTest {
 
     public static void main(String[] args) {
-        int iterations = 30;
+        int iterations = 40;
         Primes primes = new Primes();
         
         for(int i = 1; i<=iterations;i++){
+            //set new range to 2^i
             int range = (int)Math.pow(2, i);
+            
+            //set output for new cycle
             System.out.println("iteration: "+i);
             System.out.println("range: "+range);
+            
+            //set new range
             primes.set_range(range);
+            
+            //run the program
             primes.get_stats();
+            
+            //output the results
             primes.output();
         }
 
--- Primes.java
+++ Primes.java
@@ -1,24 +1,35 @@
 // @file: Primes.java
 // @author: scaperoth@gmail.com
-// @description: Class for prime numbers.
-// uses Function.java created by Rahul Simha
+// @description: Class that finds prime numbers
+// and prime number pairs within a range 0 - n
 // 
+// 
+
 package primetest;
 
 import java.util.*;
 
 public class Primes {
 
+    //amount of numbers to look through
     private int range;
+
+    //number of primes so far
     private int number_primes;
+
+    //count of numbers ending in 1,3,7, or 9
     private double number_end_1;
     private double number_end_3;
     private double number_end_7;
     private double number_end_9;
+
+    //count of pairs of 1 followed by i
     private double one_followed_by_1;
     private double one_followed_by_3;
     private double one_followed_by_7;
     private double one_followed_by_9;
+
+    //track whether or not last number ended in 1
     private boolean is_one = false;
 
     public Primes() {
@@ -30,9 +41,11 @@ public class Primes {
         this.number_primes = 0;
         this.range = range;
     }
-
-    //return statistics for primes from 1 to n
-    //where n is the number of primes
+    
+    /**
+     * set statistics for primes from 1 to n
+     * where n is the number of primes
+     */
     public void get_stats() {
         reset();
         int current_prime = 0;
@@ -46,17 +59,20 @@ public class Primes {
     }
 
     /**
-     * updates the statistics with prime
-     *
-     * @param prime
+     * updates the statistics with prime also checks and sets if prime number is
+     * 1 and increments each pair: 1-1, 1-3, 1-7, etc.
+     * @param prime     int
      */
     public void update_stats(int prime) {
         int ones_place = get_ones_place(prime);
 
         switch (ones_place) {
             case 1:
+                //increment count of current prime
                 number_end_1++;
+                //if the previous number was one
                 if (is_one) {
+                    //increment count of pair
                     one_followed_by_1++;
                 }
 
@@ -87,25 +103,30 @@ public class Primes {
 
     }
 
-    //find prime numbers
-    public int get_next_prime(int current_prime) {
+    /**
+     * find prime number greater than or equal to 
+     * current number
+     * @param current_number    int
+     * @return                  int
+     */
+    public int get_next_prime(int current_number) {
         int next_prime = 0;
 
         //check to make sure current prime is valid
-        if (current_prime <= 1) {
+        if (current_number <= 1) {
             return -1;
         }
 
         //iterate each number within range bound
         do {
             //check if current number is prime
-            if (is_prime(current_prime)) {
+            if (is_prime(current_number)) {
                 //increase number of primes found
                 number_primes++;
-                next_prime = current_prime;
+                next_prime = current_number;
             }
             //if it's not, increment current number
-            current_prime += 1;
+            current_number += 1;
         } while (next_prime == 0);
 
         //return the prime number
@@ -115,9 +136,8 @@ public class Primes {
     /**
      * returns whether or not number is prime
      * http://www.mkyong.com/java/how-to-determine-a-prime-number-in-java/
-     *
      * @param prime_to_test int
-     * @return boolean
+     * @return              boolean
      */
     private boolean is_prime(int prime_to_test) {
 
@@ -138,9 +158,8 @@ public class Primes {
     /**
      * returns the ones place of a number using recursion
      * http://stackoverflow.com/questions/9962420/how-to-get-the-place-of-a-number-in-java-eg-tens-thousands-etc
-     *
-     * @param number int
-     * @return int
+     * @param number    int
+     * @return          int
      */
     public int get_ones_place(int number) {
 
@@ -160,6 +179,9 @@ public class Primes {
         return place;
     }
 
+    /**
+     * resets variables
+     */
     private void reset() {
         number_primes = 0;
         number_end_1 = 0;
@@ -173,28 +195,40 @@ public class Primes {
         one_followed_by_9 = 0;
     }
 
-    //set range
+    /**
+     * set range
+     * @param range int
+     */
     public void set_range(int range) {
         this.range = range;
     }
 
-    //get range
+    /**
+     * get range
+     * @return  int 
+     */
     public int get_range() {
         return this.range;
     }
 
-    //get number of primes
+    /**
+     * get number of primes so far
+     * @return  int
+     */
     public int get_number_primes() {
         return this.number_primes;
     }
 
+    /**
+     * Outputs results of program
+     */
     public void output() {
 
         System.out.println("#1's: " + number_end_1 / number_primes * 100 + "%");
         System.out.println("#3's: " + number_end_3 / number_primes * 100 + "%");
         System.out.println("#7's: " + number_end_7 / number_primes * 100 + "%");
         System.out.println("#9's: " + number_end_9 / number_primes * 100 + "%");
-        
+
         System.out.println("1's following 1: " + one_followed_by_1 / number_end_1 * 100 + "%");
         System.out.println("3's following 1: " + one_followed_by_3 / number_end_1 * 100 + "%");
         System.out.println("7's following 1: " + one_followed_by_7 / number_end_1 * 100 + "%");
